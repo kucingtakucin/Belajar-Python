133aef9f129dd212ccabca1587580b02e2486f05
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2019-12-27 15:22:42 +0700

    Materi baru teman-teman

---
+++ helloadam/01HelloWorld/__init__.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Hai namaku adam, biasa dipanggil arthur")
+print("My nickname is Mbah Putih Mulyosugito")
+print("Mantab-mantab")
---
+++ helloadam/02Aritmetika/__init__.py
@@ -0,0 +1,24 @@
+print("===== OPERATOR ARITMETIKA =====\n")
+print("--- Penjumlahan ---")
+a = 5;
+b = 5;
+c = a + b;
+print(a, " + ", b, " = ", c);
+
+print("--- Pengurangan ---")
+a = 5
+b = 5
+c = a - b
+print(a, " - ", b, " = ", c)
+
+print("--- Perkalian ---")
+a = 5
+b = 5
+c = a * b
+print(a, " * ", b, " = ", c)
+
+print("--- Pembagian ---")
+a = 5
+b = 5
+c = a / b
+print(a, " / ", b, " = ", c)
