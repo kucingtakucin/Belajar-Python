617eade879eaa3fc092c93369ef45cd7759d801b
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2019-12-28 13:42:55 +0700

    Menambah materi baru

---
+++ helloadam/03Strings/__init__.py
@@ -0,0 +1,21 @@
+print("====== STRING ======")
+
+print("Hello World")
+text = 'hello dunia'
+text2 = "halo dunia"
+print(text, "&", text2)
+
+text3 = """
+Halo adam
+Adam Arthur Faizal
+"""
+print(text3)
+
+text4 = 'Mbah \'putih\' Mulyosugito'
+text5 = "Mbah \"Putih\" Mulyosugito"
+print(text4, "&", text5)
+
+text6 = "Ini adalah enter \nIni adalah enter"
+print(text6)
+text7 = r"Ini bukan enter \nIni bukan enter"
+print(text7)
