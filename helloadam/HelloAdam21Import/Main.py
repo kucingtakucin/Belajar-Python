65c0ec673021e1f41604f7f5cd7b8ce255f392a7
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2019-12-31 20:52:19 +0700

    Mengubah keseluruhan nama file serta menambah materi looping, import, dan modul

---
+++ helloadam/HelloAdam21Import/Main.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+print("Adam Arthur Faizal")
+print("Mbah Putih Mulyosugito")
+NIM = "M3119001"
+jurusan = "Teknik Informatika"
+
+def UNS():
+    print("Fakultas Matematika & Ilmu Pengetahuan Alam")
